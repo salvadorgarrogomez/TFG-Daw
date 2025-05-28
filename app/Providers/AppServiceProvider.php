55d397e56469f46f929145171a1c9f482d02c879
salvador <salvador@o2w.es>
2025-05-28 19:54:57 +0200

    providers para http a https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        if ($this->app->environment('production')) {
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
