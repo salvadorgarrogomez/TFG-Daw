245f7f31ad53d4bb5142d2791f872fc2ef235dca
salvador <salvador@o2w.es>
2025-05-31 16:14:43 +0200

    Ajuste vista comandas, para visibilidad

--- frontend/comandas/core.cljs
+++ frontend/comandas/core.cljs
@@ -1,27 +1,44 @@
 (ns comandas.core
   (:require [reagent.core :as r]
-            [app.state :as state]))
+            [reagent.dom :as dom]
+            [app.state :as state]
+            [administracion.core :as core]))
 
 (defn page []
-  (if (state/rol-admin?)
-    (r/create-class
-     {:component-did-mount
-      (fn []
-        (js/console.log "Componente montado."))
-      :reagent-render
-      (fn []
-        [:div.row {:class "divPanelImagenes"}
-         [:div.col-12 {:class "volverAtras"}
-          [:button
-           {:on-click #(set! (.-hash js/location) "#/administracion")}
-           "Volver al panel de Administración"]]
-         [:div.row {:class "adminImagenes"}
-          [:div.col.12
-           [:h1 "Apuntar comandas:"]]]])})
-    ;; Mostrar mensaje si el acceso no está permitido
-    [:div.alert.alert-danger
-     [:h4 "⚠️ Acceso denegado"]
-     [:p "Para acceder a esta seccion, debes logearte y acceder desde su boton determinado."]
-     [:button
-      {:on-click #(set! (.-hash js/location) "#/administracion")}
-      "LOGIN"]]))
+  (fn []
+    (cond
+      ;; Aún no se ha verificado la sesión (espera sin hacer nada)
+      (not @core/sesion-verificada?)
+      [:div "Cargando..."] ;; o nil
+      ;; No hay rol definido después de la verificación → redirige
+      (nil? @state/rol-usuario)
+      (do
+        (set! (.-hash js/location) "#/administracion")
+        nil)
+      ;; No tiene permisos → redirige
+      (not (or (state/rol-admin?) (state/rol-estandar?)))
+      (do
+        (set! (.-hash js/location) "#/administracion")
+        nil)
+      ;; Si todo está bien, muestra el contenido
+      :else
+      (r/create-class
+       {:component-did-mount
+        (fn []
+          (js/console.log "Componente montado."))
+        :reagent-render
+        (fn []
+          [:div.row {:class "divPanelImagenes"}
+           [:div.col-12 {:class "volverAtras"}
+            [:button
+             {:on-click #(set! (.-hash js/location) "#/administracion")}
+             "Volver al panel de Administración"]]
+           [:div.row {:class "adminImagenes"}
+            [:div.col-12
+             [:h1 "Apuntar comandas:"]]]])})))) ;; <- ojo, cierro todos los paréntesis correctamente aquí
+
+(defn init []
+  (core/verificar-sesion)
+  (dom/render [page] (.getElementById js/document "app")))
+
+(init)
