e8a6d666d5bc2a6f5dbe6e462db475309e408820
salvador <salvador@o2w.es>
2025-06-11 12:25:35 +0200

    Ajuste de estilos para pantalla movil

--- frontend/categorias/core.cljs
+++ frontend/categorias/core.cljs
@@ -47,27 +47,29 @@
            :aria-expanded "false"}
           "Selecciona una categoría de la carta"]
          [:ul.dropdown-menu
-          (for [categoria @categorias]
-            ^{:key (:id categoria)}
-            [:li
-             [:a.dropdown-item
-              {:class (when (= @categoria-seleccionada (:id categoria)) "active")
-               :on-click #(do
-                            (reset! categoria-seleccionada (:id categoria))
-                            (obtener-productos (:id categoria))
-                            (reset! mensaje-categoria (obtener-descripcion-categoria (:id categoria))))}
-              (:nombre categoria)]])]]
+          (doall
+           (for [categoria @categorias]
+             ^{:key (:id categoria)}
+             [:li
+              [:a.dropdown-item
+               {:class (when (= @categoria-seleccionada (:id categoria)) "active")
+                :on-click #(do
+                             (reset! categoria-seleccionada (:id categoria))
+                             (obtener-productos (:id categoria))
+                             (reset! mensaje-categoria (obtener-descripcion-categoria (:id categoria))))}
+               (:nombre categoria)]]))]]
         ;; Botones horizontales visibles en pantallas ≥ sm
         [:div.d-none.d-sm-flex.justify-content-center.flex-wrap.gap-2.mt-3
-         (for [categoria @categorias]
-           ^{:key (:id categoria)}
-           [:button.buttons
-            {:class (when (= @categoria-seleccionada (:id categoria)) "selected")
-             :on-click #(do
-                          (reset! categoria-seleccionada (:id categoria))
-                          (obtener-productos (:id categoria))
-                          (reset! mensaje-categoria (obtener-descripcion-categoria (:id categoria))))}
-            (:nombre categoria)])]
+         (doall
+          (for [categoria @categorias]
+            ^{:key (:id categoria)}
+            [:button.buttons
+             {:class (when (= @categoria-seleccionada (:id categoria)) "selected")
+              :on-click #(do
+                           (reset! categoria-seleccionada (:id categoria))
+                           (obtener-productos (:id categoria))
+                           (reset! mensaje-categoria (obtener-descripcion-categoria (:id categoria))))}
+             (:nombre categoria)]))]
         ;; Mensaje inferior, descripcion de cada categoria
         [:div
          (if (not-empty @mensaje-categoria)
@@ -80,76 +82,77 @@
             [:p.col-12]
             [:ul.row {:class "productosUL"}
              (let [productos-agrupados (agrupar-productos @productos)]
-               (for [[nombre lista-productos] productos-agrupados]
-                 ^{:key nombre}
-                 [:li.col-12 {:class "productosLI"}
-                  [:div {:class "producto-info"}
-                   [:div
-                    [:h4 nombre]
-                    (let [primer-producto (first lista-productos)
-                          descripcion-limpia (str/trim (:description primer-producto))
-                          alergenos {"contiene_gluten" "/imgs/alergenos/gluten.png"
-                                     "contiene_crustaceos" "/imgs/alergenos/crustaceos.png"
-                                     "contiene_huevos" "/imgs/alergenos/huevos.png"
-                                     "contiene_pescado" "/imgs/alergenos/pescado.png"
-                                     "contiene_cacahuetes" "/imgs/alergenos/cacahuetes.png"
-                                     "contiene_soja" "/imgs/alergenos/soja.png"
-                                     "contiene_lacteos" "/imgs/alergenos/lacteos.png"
-                                     "contiene_frutos_de_cascara" "/imgs/alergenos/cascaras.png"
-                                     "contiene_apio" "/imgs/alergenos/apio.png"
-                                     "contiene_mostaza" "/imgs/alergenos/mostaza.png"
-                                     "contiene_granos_de_sesamo" "/imgs/alergenos/sesamo.png"
-                                     "contiene_sulfitos" "/imgs/alergenos/sulfitos.png"
-                                     "contiene_moluscos" "/imgs/alergenos/moluscos.png"
-                                     "contiene_altramuces" "/imgs/alergenos/altramuces.png"}
-                          alergenos-activos (filter #(true? (get primer-producto (keyword (first %)))) alergenos)] ;; Filtramos los alérgenos activos
-                      [:div
-                       [:p {:class "description"}
-                        (if (empty? descripcion-limpia)
-                          " "
-                          descripcion-limpia)]
-                       (when (seq alergenos-activos) ;; Solo mostramos los alérgenos si hay alguno activo
-                         [:div {:class "alergenos"}
-                          (for [[nombre-img src-img] alergenos-activos]
-                            ^{:key nombre-img}
-                            [:img {:src src-img
-                                   :alt nombre-img
-                                   :title (str (clojure.string/replace nombre-img "_" " "))
-                                   :class "alergeno-icono"}])])])]
-                   (for [producto lista-productos]
-                     ^{:key (:id producto)}
-                     [:div {:class "producto-item"} ;; Div donde en su interior ira todo el contenido de cada producto
-                      [:div {:class "precio-racion"} ;; Contenedor que agrupa precio y ración
-                       [:h2 {:class "precio"} (str (:precio producto) "€ ")]
-                       (let [tipo-porcion (:tipo_porcion producto)
-                             producto-id (:id producto)]  ;; Se guarda el ID de producto, al dar en las imagenes, saldra un alert en el navegador para ver el id
-                         (cond
-                           ;; Segun la porcion obtenida desde el json enviado por el backend, revisamos con este cond, para segun la porcion, se mostrara una imagen u otra
-                           (= tipo-porcion "Media ración")
-                           [:img {:class "racion"
-                                  :src "/imgs/medio-circulo.png"
-                                  :alt "Media ración"
-                                  :style {:cursor "pointer"}
-                                  :on-click #(js/alert (str "ID del producto: " producto-id " , Media ración"))}]
-                           (= tipo-porcion "Ración completa")
-                           [:img {:class "racion"
-                                  :src "/imgs/completo-circulo.png"
-                                  :alt "Ración completa"
-                                  :style {:cursor "pointer"}
-                                  :on-click #(js/alert (str "ID del producto: " producto-id " , Ración completa"))}]
-                           (= tipo-porcion "Por unidad")
-                           [:img {:class "racion"
-                                  :src "/imgs/unidad.png"
-                                  :alt "/Unidad"
-                                  :style {:cursor "pointer"}
-                                  :on-click #(js/alert (str "ID del producto: " (:id producto) " , Por unidad"))}]
-                           (= tipo-porcion "")
-                           [:img {:class "racion"
-                                  :src "/imgs/unidad.png"
-                                  :alt ""
-                                  :style {:cursor "pointer"}
-                                  :on-click #(js/alert (str "ID del producto: " producto-id))}]
-                           :else [:span (str "Tipo: " tipo-porcion)]))]])]]))])]]]])}))
+               (doall
+                (for [[nombre lista-productos] productos-agrupados]
+                  ^{:key nombre}
+                  [:li.col-12 {:class "productosLI"}
+                   [:div {:class "producto-info"}
+                    [:div
+                     [:h4 nombre]
+                     (let [primer-producto (first lista-productos)
+                           descripcion-limpia (str/trim (:description primer-producto))
+                           alergenos {"contiene_gluten" "/imgs/alergenos/gluten.png"
+                                      "contiene_crustaceos" "/imgs/alergenos/crustaceos.png"
+                                      "contiene_huevos" "/imgs/alergenos/huevos.png"
+                                      "contiene_pescado" "/imgs/alergenos/pescado.png"
+                                      "contiene_cacahuetes" "/imgs/alergenos/cacahuetes.png"
+                                      "contiene_soja" "/imgs/alergenos/soja.png"
+                                      "contiene_lacteos" "/imgs/alergenos/lacteos.png"
+                                      "contiene_frutos_de_cascara" "/imgs/alergenos/cascaras.png"
+                                      "contiene_apio" "/imgs/alergenos/apio.png"
+                                      "contiene_mostaza" "/imgs/alergenos/mostaza.png"
+                                      "contiene_granos_de_sesamo" "/imgs/alergenos/sesamo.png"
+                                      "contiene_sulfitos" "/imgs/alergenos/sulfitos.png"
+                                      "contiene_moluscos" "/imgs/alergenos/moluscos.png"
+                                      "contiene_altramuces" "/imgs/alergenos/altramuces.png"}
+                           alergenos-activos (filter #(true? (get primer-producto (keyword (first %)))) alergenos)] ;; Filtramos los alérgenos activos
+                       [:div
+                        [:p {:class "description"}
+                         (if (empty? descripcion-limpia)
+                           " "
+                           descripcion-limpia)]
+                        (when (seq alergenos-activos) ;; Solo mostramos los alérgenos si hay alguno activo
+                          [:div {:class "alergenos"}
+                           (for [[nombre-img src-img] alergenos-activos]
+                             ^{:key nombre-img}
+                             [:img {:src src-img
+                                    :alt nombre-img
+                                    :title (str (clojure.string/replace nombre-img "_" " "))
+                                    :class "alergeno-icono"}])])])]
+                    (for [producto lista-productos]
+                      ^{:key (:id producto)}
+                      [:div {:class "producto-item"} ;; Div donde en su interior ira todo el contenido de cada producto
+                       [:div {:class "precio-racion"} ;; Contenedor que agrupa precio y ración
+                        [:h2 {:class "precio"} (str (:precio producto) "€ ")]
+                        (let [tipo-porcion (:tipo_porcion producto)
+                              producto-id (:id producto)]  ;; Se guarda el ID de producto, al dar en las imagenes, saldra un alert en el navegador para ver el id
+                          (cond
+                            ;; Segun la porcion obtenida desde el json enviado por el backend, revisamos con este cond, para segun la porcion, se mostrara una imagen u otra
+                            (= tipo-porcion "Media ración")
+                            [:img {:class "racion"
+                                   :src "/imgs/medio-circulo.png"
+                                   :alt "Media ración"
+                                   :style {:cursor "pointer"}
+                                   :on-click #(js/alert (str "ID del producto: " producto-id " , Media ración"))}]
+                            (= tipo-porcion "Ración completa")
+                            [:img {:class "racion"
+                                   :src "/imgs/completo-circulo.png"
+                                   :alt "Ración completa"
+                                   :style {:cursor "pointer"}
+                                   :on-click #(js/alert (str "ID del producto: " producto-id " , Ración completa"))}]
+                            (= tipo-porcion "Por unidad")
+                            [:img {:class "racion"
+                                   :src "/imgs/unidad.png"
+                                   :alt "/Unidad"
+                                   :style {:cursor "pointer"}
+                                   :on-click #(js/alert (str "ID del producto: " (:id producto) " , Por unidad"))}]
+                            (= tipo-porcion "")
+                            [:img {:class "racion"
+                                   :src "/imgs/unidad.png"
+                                   :alt ""
+                                   :style {:cursor "pointer"}
+                                   :on-click #(js/alert (str "ID del producto: " producto-id))}]
+                            :else [:span (str "Tipo: " tipo-porcion)]))]])]])))])]]]])}))
 
 
 ;; Inicio de la aplicación
